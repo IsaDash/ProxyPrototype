57dcabe57b3d3d1c7722cde4643fb30a96bd8001
Isa Dash <isa.dash@cockroachlabs.com>
2021-07-26 11:42:58 -0700

    added certs

--- client/client.go
+++ client/client.go
@@ -2,24 +2,54 @@ package main
 
 import (
 	"bufio"
+	"crypto/x509"
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 )
 
 func main()  {
 	reader := bufio.NewReader(os.Stdin)
-	fmt.Println("What is your name: ")
-	name, _ := reader.ReadString('\n')
-	name = name[:len(name) - 1]
-	fmt.Println("What is your email: ")
-	email, _ := reader.ReadString('\n')
-	email = email[:len(email) - 1]
+	fmt.Println("Request: ")
+	request, _ := reader.ReadString('\n')
+	request = request[:len(request) - 1]
+	fmt.Println("URI: ")
+	URI, _ := reader.ReadString('\n')
+	URI = URI[:len(URI) - 1]
+
+	// load client cert and key pair
+	//clientCert, err := tls.LoadX509KeyPair("client1.crt", "client1.key")
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+
+	// load ca cert
+	caCert, err := ioutil.ReadFile("../nginx/certs/ca.crt")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//append ca cert to cert pool
+	caCertPool := x509.NewCertPool()
+	caCertPool.AppendCertsFromPEM(caCert)
+
+	// tls config for client
+	//tlsConf := &tls.Config{
+	//	Certificates: []tls.Certificate{clientCert},
+	//	//RootCAs: caCertPool, // root certificate authorities used when verifying server certs
+	//	InsecureSkipVerify: true, // when set to false, tls will verify server's certificate chain and host name
+	//}
+
+	// http Transport for supporting HTTP proxies and manage underlying tcp connection
+	//transport := &http.Transport{TLSClientConfig: tlsConf}
 
 	client := &http.Client{}
 	req, _ := http.NewRequest("GET", "http://localhost:7000", nil)
-	req.Header.Set("name", name)
-	req.Header.Set("email", email)
+	req.Header.Set("request", request)
+	req.Header.Set("URI", URI)
+
 	res, err := client.Do(req)
 	if err != nil {
 		panic(err)
--- server/server.go
+++ server/server.go
@@ -2,14 +2,32 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"net/http"
 )
 
 func hello(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintf(w, "hello\n")
-	for name, value := range req.Header {
-		fmt.Println(name, value)
-	}
+	//for name, value := range req.Header {
+	//	fmt.Println(name, value)
+	//}
+
+	// get client specific headers that were set in nginx proxy
+	realIp := req.Header.Get("X-Real-IP")
+	fowardedIp := req.Header.Get("X-Forwarded-For")
+	verified := req.Header.Get("VERIFIED")
+	dn := req.Header.Get("DN")
+
+	fmt.Println(realIp)
+	fmt.Println(fowardedIp)
+	fmt.Println(verified)
+	fmt.Println(dn)
+
+	resp := `{"status": "CREATED"}`
+	w.WriteHeader(http.StatusCreated)
+	w.Header().Set("Content-Type", "application/json")
+	io.WriteString(w, string(resp))
+	return
 }
 
 func main() {
